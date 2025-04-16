db1c4c60f68dc5fb369b04c62092bc0198d33a6d
薛梦石 <1801455515@qq.com>
2025-04-16 09:43:47 +0800

    加入es搜索，实现es搜索题库

---
+++ src/main/java/com/xue/mianshi/config/ElasticSearchConfig.java
@@ -0,0 +1,22 @@
+package com.xue.mianshi.config;
+
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+/**
+ * es配置
+ */
+@Component
+public class ElasticSearchConfig {
+
+    @Bean
+    public RestHighLevelClient restHighLevelClient(){
+        return new RestHighLevelClient(RestClient.builder(
+                HttpHost.create("http://localhost:9200")));
+    }
+}
